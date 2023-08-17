6b1e88a2986fda1983ea97c9e05f4631813f951c
Dvrcksoft <dvrcksoft@gmail.com>
2023-08-17 23:14:38 +0300

    asd

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aleksandrvasilenko/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aleksandrvasilenko/Downloads/iosa"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.18"
+export "FLUTTER_BUILD_NUMBER=18"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/aleksandrvasilenko/Downloads/iosa/.dart_tool/package_config.json"
